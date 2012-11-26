37c2be3c84c5739535ed8005d8e48c91ce4dd326
Jeff Xiong <gigix1980@gmail.com>
2012-11-26 22:16:11 +0800

    A simplest AngularJS page is functioning

---
+++ app/assets/javascripts/controllers.js
@@ -0,0 +1,10 @@
+function BookListCtrl($scope) {
+  $scope.books = [
+    {"title": "精益医院",
+     "isbn": "9787111338536",
+	 "douban_id": "6307962"},
+    {"title": "The New Rulers of the World",
+     "isbn": "9781859844120",
+	 "douban_id": "2311321"}
+  ];
+}
---
+++ spec/smoke_spec.rb
@@ -0,0 +1,5 @@
+describe 'rspec' do
+  it 'runs' do
+    1.should == 1
+  end
+end
