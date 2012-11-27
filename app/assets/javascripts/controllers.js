4994ddbb1dc7f0ef374eeda6dcbd216646292f27
Jeff Xiong <gigix1980@gmail.com>
2012-11-27 19:53:39 +0800

    bookshelf layout

--- app/assets/javascripts/controllers.js
+++ app/assets/javascripts/controllers.js
@@ -1,10 +1,12 @@
-function BookListCtrl($scope) {
+function BookShelfCtrl($scope) {
   $scope.books = [
     {"title": "精益医院",
      "isbn": "9787111338536",
-	 "douban_id": "6307962"},
+	 "douban_id": "6307962", 
+	 "cover_url": "http://img5.douban.com/lpic/s5779859.jpg"},
     {"title": "The New Rulers of the World",
      "isbn": "9781859844120",
-	 "douban_id": "2311321"}
+	 "douban_id": "2311321",
+	 "cover_url": "http://img3.douban.com/lpic/s2768790.jpg"}
   ];
 }
